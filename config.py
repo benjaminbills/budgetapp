a3428ee8e96db890a49619dbb8ea540651a4bab2
Benjamin Obafemi <benjaminoba234@gmail.com>
2021-05-04 11:08:14 +0300

    add authentication db uri

--- config.py
+++ config.py
@@ -1,20 +1,23 @@
 import os
 import re
 
+
 class Config:
-  '''
-  General configuration parent class
-  '''
-  SECRET_KEY = os.environ.get("SECRET_KEY")
+    '''
+    General configuration parent class
+    '''
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+
 
 class ProdConfig(Config):
-  """
-  Production  configuration child class
+    """
+    Production  configuration child class
 
-  Args:
-      Config: The parent configuration class with General configuration settings
-  """
-  pass
+    Args:
+        Config: The parent configuration class with General configuration settings
+    """
+    pass
+
 
 class DevConfig(Config):
     """
@@ -23,11 +26,15 @@ class DevConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     """
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://obafemi:Bentamjay1@localhost/login'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://obafemi:Bentamjay1@localhost/budgetapp'
 
     DEBUG = True
 
+
 class TestConfig(Config):
-    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://obafemi:Bentamjay1@localhost/login_test'
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://obafemi:Bentamjay1@localhost/budgetapp_test'
     pass
-config_options = {"development": DevConfig, "production": ProdConfig, 'test':TestConfig}
+
+
+config_options = {"development": DevConfig,
+                  "production": ProdConfig, 'test': TestConfig}
